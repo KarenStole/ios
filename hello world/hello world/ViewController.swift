6c6f2e0db7f2b247ee02c6a7cad88301ca9bbfff
KarenStole <33491102+KarenStole@users.noreply.github.com>
2019-03-14 21:23:07 -0300

    Add files via upload

---
+++ hello world/hello world/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  hello world
+//
+//  Created by Administrador on 14/3/19.
+//  Copyright © 2019 UCUDAL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
